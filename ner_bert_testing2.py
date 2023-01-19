b9258e79a71a521037124cbf43ecd377617a77e6
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-19 14:31:09 +0530

    Update ner_bert_testing2.py

--- ner_bert_testing2.py
+++ ner_bert_testing2.py
@@ -415,9 +415,9 @@ print(len(true_enc_tag))
 
 
 
-from sklearn.metrics import accuracy_score,classification_report,f1_score
-print(accuracy_score(true_enc_tag,pred_enc_tag))
-print(classification_report(true_enc_tag,pred_enc_tag))
+# from sklearn.metrics import accuracy_score,classification_report,f1_score
+# print(accuracy_score(true_enc_tag,pred_enc_tag))
+# print(classification_report(true_enc_tag,pred_enc_tag))
 
 list_ones = []
 def model(pred, actual):

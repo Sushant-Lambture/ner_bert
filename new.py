b48bc25c36545c378a2bfacd1c8231153a641c9a
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-23 11:55:02 +0530

    Update new.py

--- new.py
+++ new.py
@@ -30,7 +30,7 @@ from tqdm import tqdm
 from torch.optim import SGD
 
 import os
-TOKENIZERS_PARALLELISM=True
+TOKENIZERS_PARALLELISM=False
 
 df = pd.read_csv(r"train_set2.csv")
 df.rename({'word':'text','label':'labels'},axis=1,inplace=True)
@@ -210,6 +210,96 @@ BATCH_SIZE = 2
 model = BertModel()
 train_loop(model, df_train, df_val)
 
+
+
+tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
+
+label_all_tokens = False
+
+def align_label(texts, labels):
+    tokenized_inputs = tokenizer(texts, padding='max_length', max_length=512, truncation=True)
+
+    word_ids = tokenized_inputs.word_ids()
+
+    previous_word_idx = None
+    label_ids = []
+
+    for word_idx in word_ids:
+
+        if word_idx is None:
+            label_ids.append(-100)
+
+        elif word_idx != previous_word_idx:
+            try:
+                label_ids.append(labels_to_ids[labels[word_idx]])
+            except:
+                label_ids.append(-100)
+        else:
+            try:
+                label_ids.append(labels_to_ids[labels[word_idx]] if label_all_tokens else -100)
+            except:
+                label_ids.append(-100)
+        previous_word_idx = word_idx
+
+    return label_ids
+
+class DataSequence(torch.utils.data.Dataset):
+
+    def __init__(self, test):
+
+        lb = [i.split() for i in df['labels'].values.tolist()]
+        txt = df['text'].values.tolist()
+        self.texts = [tokenizer(str(i),
+                               padding='max_length', max_length = 512, truncation=True, return_tensors="pt") for i in txt]
+        self.labels = [align_label(i,j) for i,j in zip(txt, lb)]
+
+    def __len__(self):
+
+        return len(self.labels)
+
+    def get_batch_data(self, idx):
+
+        return self.texts[idx]
+
+    def get_batch_labels(self, idx):
+
+        return torch.LongTensor(self.labels[idx])
+
+    def __getitem__(self, idx):
+
+        batch_data = self.get_batch_data(idx)
+        batch_labels = self.get_batch_labels(idx)
+
+        return batch_data, batch_labels
+
+# df = df[0:1000]
+test=test
+
+labels = [i.split() for i in df['labels'].values.tolist()]
+unique_labels = set()
+
+for lb in labels:
+        [unique_labels.add(i) for i in lb if i not in unique_labels]
+labels_to_ids = {k: v for v, k in enumerate(unique_labels)}
+ids_to_labels = {v: k for v, k in enumerate(unique_labels)}
+
+# df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42),
+#                             [int(.8 * len(df)), int(.9 * len(df))])
+
+class BertModel(torch.nn.Module):
+
+    def __init__(self):
+
+        super(BertModel, self).__init__()
+
+        self.bert = BertForTokenClassification.from_pretrained('bert-base-cased', num_labels=len(unique_labels))
+
+    def forward(self, input_id, mask, label):
+
+        output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
+
+        return output
+
 def evaluate(model, df_test):
 
     test_dataset = DataSequence(df_test)
@@ -246,7 +336,7 @@ def evaluate(model, df_test):
     print(f'Test Accuracy: {total_acc_test / len(df_test): .3f}')
 
 
-evaluate(model, test)
+evaluate(model, test[:100])
 
 def align_word_ids(texts):
   

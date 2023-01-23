21c89eea4dc930c38b818a373877eaa250ca0a3e
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-23 11:07:11 +0530

    Update new.py

--- new.py
+++ new.py
@@ -585,3 +585,58 @@ def evaluate(model, df_test):
 
 
 evaluate(model, test[:100])
+
+
+def align_word_ids(texts):
+  
+    tokenized_inputs = tokenizer(texts, padding='max_length', max_length=512, truncation=True)
+
+    word_ids = tokenized_inputs.word_ids()
+
+    previous_word_idx = None
+    label_ids = []
+
+    for word_idx in word_ids:
+
+        if word_idx is None:
+            label_ids.append(-100)
+
+        elif word_idx != previous_word_idx:
+            try:
+                label_ids.append(1)
+            except:
+                label_ids.append(-100)
+        else:
+            try:
+                label_ids.append(1 if label_all_tokens else -100)
+            except:
+                label_ids.append(-100)
+        previous_word_idx = word_idx
+
+    return label_ids
+
+
+def evaluate_one_text(model, sentence):
+
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+        model = model.cuda()
+
+    text = tokenizer(sentence, padding='max_length', max_length = 512, truncation=True, return_tensors="pt")
+
+    mask = text['attention_mask'].to(device)
+    input_id = text['input_ids'].to(device)
+    label_ids = torch.Tensor(align_word_ids(sentence)).unsqueeze(0).to(device)
+
+    logits = model(input_id, mask, None)
+    logits_clean = logits[0][label_ids != -100]
+
+    predictions = logits_clean.argmax(dim=1).tolist()
+    prediction_label = [ids_to_labels[i] for i in predictions]
+    print(sentence)
+    print(prediction_label)
+            
+evaluate_one_text(model, 'ajay sushant')

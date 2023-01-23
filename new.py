662d7cfa481648e745574f36748471ceb4cc1c15
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-23 10:11:36 +0530

    Update new.py

--- new.py
+++ new.py
@@ -32,17 +32,17 @@ from torch.optim import SGD
 import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-# df = pd.read_csv("test_set_ran.csv")
-# df.rename({'word':'text','label':'labels'},axis=1,inplace=True)
-# df = df.drop(['Unnamed: 0.1','Unnamed: 0'],axis=1)
-# # df = df.drop('index',axis=1)
-# df.head()
-
-df = pd.read_csv("train_final.csv")
+df = pd.read_csv("test_set_ran.csv")
 df.rename({'word':'text','label':'labels'},axis=1,inplace=True)
-df = df.drop('Unnamed: 0',axis=1)
+df = df.drop(['Unnamed: 0.1','Unnamed: 0'],axis=1)
 # df = df.drop('index',axis=1)
 df.head()
+
+# df = pd.read_csv("train_final.csv")
+# df.rename({'word':'text','label':'labels'},axis=1,inplace=True)
+# df = df.drop('Unnamed: 0',axis=1)
+# # df = df.drop('index',axis=1)
+# df.head()
 
 tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
 

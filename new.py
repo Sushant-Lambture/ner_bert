a89a354413399ecc92dc1db46b097b3c3e2f5c57
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-23 19:04:45 +0530

    Update new.py

--- new.py
+++ new.py
@@ -38,6 +38,7 @@ df = pd.read_csv(r"train_final.csv")
 df.rename({'word':'text','label':'labels'},axis=1,inplace=True)
 # df = df.drop('index',axis=1)
 df = df.drop(['Unnamed: 0'],axis=1)
+df = df.dropna()
 
 # 2. Test dataset
 test = pd.read_csv("test_set_ran.csv")

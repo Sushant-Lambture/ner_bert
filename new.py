cb7027a2eb59bfe77b01593f36b9d4eade16b30d
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-24 13:25:32 +0530

    Update new.py

--- new.py
+++ new.py
@@ -112,7 +112,7 @@ class DataSequence(torch.utils.data.Dataset):
 
         return batch_data, batch_labels
 
-df = df[:200000]
+df = df[:150000]
 
 labels = [i.split() for i in df['labels'].values.tolist()]
 unique_labels = set()

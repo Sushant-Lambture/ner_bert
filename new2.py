a30dd4f6f548f374950ac6727f4139e45c96a9ae
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-01-30 15:57:09 +0530

    Update new2.py

--- new2.py
+++ new2.py
@@ -65,7 +65,7 @@ tag2idx # indexing the tag
 import torch
 from torch.optim import Adam
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
-from keras.preprocessing.sequence import pad_sequences
+from keras_preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 from pytorch_pretrained_bert import BertTokenizer, BertConfig
 from pytorch_pretrained_bert import BertForTokenClassification, BertAdam

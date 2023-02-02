353c7079dcdd373663394ee68240d171f3a5465e
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-02-02 12:48:48 +0530

    Update ner_bert_testing1.py

--- ner_bert_testing1.py
+++ ner_bert_testing1.py
@@ -176,6 +176,21 @@ with open("model.json", "w") as json_file:
 model.save_weights("model.h5")
 print("Saved model to disk")
 
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
+
+
 # model.save_weights("ner_bert_weights")
 
 

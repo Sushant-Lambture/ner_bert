1d50932277777f07a329667b3ea8c5d99bf6cf6b
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-02-02 12:54:53 +0530

    Update ner_bert_testing1.py

--- ner_bert_testing1.py
+++ ner_bert_testing1.py
@@ -12,6 +12,10 @@ except:
 print('Number of replicas:', strategy.num_replicas_in_sync)
 
 # pip install transformers
+ evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
 
 import numpy as np
 import pandas as pd
@@ -180,7 +184,7 @@ print("Saved model to disk")
 json_file = open('model.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
-loaded_model = model_from_json(loaded_model_json)
+loaded_model = keras.models.model_from_json(loaded_model_json)
 # load weights into new model
 loaded_model.load_weights("model.h5")
 print("Loaded model from disk")

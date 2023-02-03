877695b726a5e50cdc7e35e7bf71fd27fe2724e7
Sushant-Lambture <107694488+Sushant-Lambture@users.noreply.github.com>
2023-02-03 18:29:30 +0530

    Update ner_bert_testing2.py

--- ner_bert_testing2.py
+++ ner_bert_testing2.py
@@ -169,6 +169,16 @@ with open("model.json", "w") as json_file:
 model.save_weights("model.h5")
 print("Saved model to disk")
 
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
 # model.save_weights("ner_bert_weights")
 
 # plt.plot(history_bert.history['accuracy'])
